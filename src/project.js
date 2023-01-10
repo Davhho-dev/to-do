bb2ca67f306b8dd40c1da506ee41e488ef51d08b
Davhho-dev <Davhho@utexas.edu>
2023-01-09 20:38:42 -0600

    Add displayProject function when form is submitted

--- src/project.js
+++ src/project.js
@@ -1,3 +1,7 @@
+import { addToProjectList, displayProject } from "./UI";
+
+let projectArr = [];
+
 function projectModal() {
     const projectModal = document.querySelector(".project-modal");
     projectModal.showModal();
@@ -8,4 +12,30 @@ function projectModal() {
     return projectModal;
 }
 
-export {projectModal};
+function projectForm() {
+    const form = document.querySelector(".project-form");
+    const projectModal = document.querySelector(".project-modal");
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const title = document.getElementById("title");
+        const description = document.getElementById("description");
+        const dueDate = document.getElementById("due-date");
+        let priority = "Low";
+        if(document.getElementById("medium").checked) priority = "Medium";
+        else if(document.getElementById("high").checked) priority = "High";
+        else priority;
+        const newProject = userProject(title.value, description.value, dueDate.value, priority);
+        projectArr.push(newProject);
+        addToProjectList(projectArr);
+        displayProject(newProject);
+        projectModal.close();
+        console.log(projectArr);
+    });
+}
+
+const userProject = (title, description, dueDate, priority) => {
+    return {title, description, dueDate, priority};
+} 
+
+
+export {projectModal, projectForm};
