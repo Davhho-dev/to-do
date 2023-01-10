e2f0edc826efe0ced0fe04e00a4eb20b6524ca0c
Davhho-dev <Davhho@utexas.edu>
2023-01-09 20:37:48 -0600

    Display project and change priority text color based on status

--- src/UI.js
+++ src/UI.js
@@ -20,4 +20,54 @@ function changeArrow(arrowDirection) {
     else return arrowIcon.innerText = "expand_more";
 }
 
-export {displayCompletedList, changeArrow};
+function addToProjectList(arr) {
+    const completedList = document.querySelector(".project-list");
+    completedList.textContent = "";
+    arr.forEach((project) => {
+        const newProjectContainer = document.createElement("div");
+        newProjectContainer.classList.add("new-project-con");
+        const projectTitle = document.createElement("p");
+        projectTitle.classList.add("project-title");
+        projectTitle.textContent = project.title;
+        newProjectContainer.appendChild(projectTitle);
+        newProjectContainer.appendChild(createProjectButtons());
+        completedList.appendChild(newProjectContainer);
+    })
+    return completedList;
+}
+
+function displayProject(projectObj) {
+    const header = document.querySelector(".main-hd");
+    header.textContent = projectObj.title;
+    const description = document.querySelector(".main-hd-desc");
+    description.textContent = projectObj.description;
+    const date = document.querySelector(".main-hd-date-user");
+    date.textContent = projectObj.dueDate;
+    const priority = document.querySelector(".main-hd-priority-user");
+    priority.textContent = projectObj.priority;
+    priority.style.color = changePriorityColor(projectObj.priority);
+}
+
+function changePriorityColor(priority) {
+    if(priority === "Low") return "green";
+    else if(priority === "Medium") return "yellow";
+    else return "red";
+}
+
+function createProjectButtons() {
+    const projectBtn = document.createElement("div");
+    projectBtn.classList.add("project-btn");
+    const editBtn = document.createElement("span");
+    const deleteBtn = document.createElement("span");
+    editBtn.classList.add("material-icons-outlined");
+    editBtn.classList.add("edit");
+    deleteBtn.classList.add("material-icons-outlined");
+    deleteBtn.classList.add("delete");
+    editBtn.textContent = "edit";
+    deleteBtn.textContent = "delete";
+    projectBtn.appendChild(editBtn);
+    projectBtn.appendChild(deleteBtn);
+    return projectBtn;
+}
+
+export {displayCompletedList, changeArrow, addToProjectList, displayProject};
