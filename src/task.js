050eabe19c2a59d1e07876e5c29444bd8e686d71
Davhho-dev <Davhho@utexas.edu>
2023-01-09 22:29:26 -0600

    Create function for task form and associating task to the correct project

--- src/task.js
+++ src/task.js
@@ -1,3 +1,5 @@
+let taskArr = [];
+
 function taskModal() {
     const taskModal = document.querySelector(".task-modal");
     taskModal.showModal();
@@ -8,4 +10,36 @@ function taskModal() {
     return taskModal;
 }
 
-export {taskModal};
+function taskForm() {
+    const form = document.querySelector(".task-form");
+    const taskModal = document.querySelector(".task-modal");
+    const title = document.querySelector(".main-hd");
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const description = document.getElementById("task-description");
+        if(!checkTaskArray(title.textContent)) {
+            const newTask = userTask(title.textContent, [description.value]);
+            taskArr.push(newTask);
+        }else {
+            const projectIndex = taskArr.findIndex(task => task.project === title.textContent);
+            taskArr[projectIndex].description.push(description.value); 
+        }
+        taskModal.close();
+        form.reset();
+        console.log(taskArr);
+        console.log(taskArr[0].description);
+    })
+}
+
+function checkTaskArray(title) {
+    if(taskArr.length === 0) return false;
+    for(let x = 0; x < taskArr.length; x++) {
+        if(taskArr[x].project === title) return true;
+    }
+}
+
+const userTask = (project, description) => {
+    return {project, description};
+} 
+
+export {taskModal, taskForm};
