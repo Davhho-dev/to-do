accd839f7911a6fd8236666230ab22312e6ab178
Davhho-dev <Davhho@utexas.edu>
2023-01-10 21:20:18 -0600

    Create click event listener when selecting a project and displaying the associated project and task on UI

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import { projectModal, projectForm } from "./project";
-import { taskModal, taskForm } from "./task";
-import { displayCompletedList, changeArrow } from "./UI";
+import { projectModal, projectForm, projectArr} from "./project";
+import { taskModal, taskForm, taskArr } from "./task";
+import { displayCompletedList, changeArrow, displayProject, displayTask} from "./UI";
 
 const addProjectBtn = document.querySelector(".add-project");
 addProjectBtn.addEventListener("click", () => {
@@ -9,6 +9,19 @@ addProjectBtn.addEventListener("click", () => {
 
 projectForm();
 taskForm();
+
+const projectList = document.querySelectorAll(".project-list");
+projectList.forEach((project) => {
+    project.addEventListener("click", (e) => {
+        const projectTitle = e.target.childNodes[0].childNodes[0].textContent;
+        const projectIndex = projectArr.findIndex(project => project.title === projectTitle);
+        displayProject(projectArr[projectIndex]);
+        const taskIndex = taskArr.findIndex(task => task.project === projectTitle);
+        if(taskIndex !== -1) displayTask(taskArr[taskIndex].description);
+        // if(taskArr) displayTask(taskArr[taskIndex].description);
+    })
+})
+
 
 const addTaskbtn = document.querySelector(".add-task");
 addTaskbtn.addEventListener("click", () => {
