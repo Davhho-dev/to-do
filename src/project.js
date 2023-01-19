a27d7092f2d70310b203482f30bcd14eb5e7f4cc
Davhho-dev <Davhho@utexas.edu>
2023-01-18 20:50:04 -0600

    Revise and deleted some functionality to keep the project barebones

--- src/project.js
+++ src/project.js
@@ -1,90 +1,6 @@
-// import { addToProjectList, displayProject } from "./UI";
-// import { updateTask } from "./task";
-
-// let projectArr = [];
-
-// function projectModal() {
-//     const projectModal = document.querySelector(".project-modal");
-//     projectModal.showModal();
-//     const closeBtn = document.querySelector(".project-close");
-//     closeBtn.addEventListener("click", () => {
-//         projectModal.close();
-//     });
-//     return projectModal;
-// }
-
-// function projectForm() {
-//     const form = document.querySelector(".project-form");
-//     const projectModal = document.querySelector(".project-modal");
-//     form.addEventListener("submit", (e) => {
-//         e.preventDefault();
-//         const title = document.getElementById("title");
-//         const description = document.getElementById("description");
-//         const dueDate = document.getElementById("due-date");
-//         let priority = "Low";
-//         if(document.querySelector(".project-modal #medium").checked) priority = "Medium";
-//         else if(document.querySelector(".project-modal #high").checked) priority = "High";
-//         else priority;
-//         const newProject = userProject(title.value, description.value, dueDate.value, priority);
-//         projectArr.push(newProject);
-//         addToProjectList(projectArr);
-//         displayProject(newProject);
-//         projectModal.close();
-//         form.reset();
-//         // console.log(projectArr);
-//         return projectArr;
-//     });
-// }
-
-// function editModal() {
-//     const editModal = document.querySelector(".edit-modal");
-//     editModal.showModal();
-//     const closeBtn = document.querySelector(".edit-close");
-//     closeBtn.addEventListener("click", () => {
-//         editModal.close();
-//     });
-//     return editModal;
-// }
-
-// function editForm(projectIndex) {
-//     const form = document.querySelector(".edit-form");
-//     const projectModal = document.querySelector(".edit-modal");
-//     form.addEventListener("submit", (e) => {
-//         e.preventDefault();
-//         const title = document.getElementById("edit-title");
-//         projectArr[projectIndex].title = title.value;
-//         const description = document.getElementById("edit-description");
-//         projectArr[projectIndex].description = description.value;
-//         const dueDate = document.getElementById("edit-due-date");
-//         projectArr[projectIndex].dueDate = dueDate.value;
-//         let priority = "Low";
-//         if(document.querySelector(".edit-modal #medium").checked) priority = "Medium";
-//         else if(document.querySelector(".edit-modal #high").checked) priority = "High";
-//         else priority;
-//         projectArr[projectIndex].priority = priority;
-//         console.log("project-module");
-//         console.log(projectArr);
-//         addToProjectList(projectArr);
-//         displayProject(projectArr[projectIndex]);
-//         updateTask(projectIndex, projectArr[projectIndex].title);
-//         projectModal.close();
-//         form.reset();
-//         return projectArr;
-//     }, {once: true}); //prevent form from submitting twice when editing more than once
-// }
-
-// function deleteProject(projectIndex) {
-//     return projectArr.splice(projectIndex, 1);
-// }
-
-// const userProject = (title, description, dueDate, priority) => {
-//     return {title, description, dueDate, priority};
-// } 
-
-
-// export {projectModal, projectForm, projectArr, editModal, editForm, deleteProject};
-
 import { displayProject, displayMain, displayDeleteMain } from "./UI";
+import { updateTask } from "./task";
+
 
 let projectArr = [];
 
@@ -114,6 +30,7 @@ function projectForm() {
 }
 
 function editProjectForm(projectTitle) {
+
     const editModal = document.querySelector(".edit-modal");
     const editForm = document.querySelector(".edit-form");
     editForm.addEventListener("submit", (e) => {
@@ -132,6 +49,7 @@ function editProjectForm(projectTitle) {
         projectArr[index].priority = priority;
         displayProject(projectArr);
         displayMain(projectArr[index]);
+        updateTask(index, title.value);
         editModal.close();
         console.log(projectArr);
         HTMLFormElement.prototype.reset.call(editForm)
@@ -154,6 +72,5 @@ function selectedProject(projectTitle) {
 function findProject(projectTitle) {
     return projectArr.findIndex(project => project.title === projectTitle);
 }
-
 
 export {projectForm, editProjectForm, deleteProject, selectedProject};
--- src/task.js
+++ src/task.js
@@ -1,104 +1,5 @@
-// import { addToTaskList, displayTask } from "./UI";
-
-// let taskArr = [];
-
-// function taskModal() {
-//     const taskModal = document.querySelector(".task-modal");
-//     taskModal.showModal();
-//     const closeBtn = document.querySelector(".task-close");
-//     closeBtn.addEventListener("click", (e) => {
-//         taskModal.close();
-//     });
-//     return taskModal;
-// }
-
-// function taskForm() {
-//     const form = document.querySelector(".task-form");
-//     const taskModal = document.querySelector(".task-modal");
-//     const title = document.querySelector(".main-hd");
-//     form.addEventListener("submit", (e) => {
-//         e.preventDefault();
-//         const description = document.getElementById("task-description");
-//         if(!checkTaskArray(title.textContent)) {
-//             const newTask = userTask(title.textContent, [description.value]);
-//             taskArr.push(newTask);
-//             const projectIndex = taskArr.findIndex(task => task.project === title.textContent);
-//             displayTask(taskArr[projectIndex].description);
-//         }else {
-//             const projectIndex = taskArr.findIndex(task => task.project === title.textContent);
-//             taskArr[projectIndex].description.push(description.value); 
-//             displayTask(taskArr[projectIndex].description);
-//         }
-//         // addToTaskList(taskArr);
-//         taskModal.close();
-//         form.reset();
-//         // console.log(taskArr);
-//         // console.log(taskArr[0].description);
-//         return taskArr;
-//     })
-// }
-
-// function editTaskModal() {
-//     const taskModal = document.querySelector(".edit-task-modal");
-//     taskModal.showModal();
-//     const closeBtn = document.querySelector(".edit-task-close");
-//     closeBtn.addEventListener("click", (e) => {
-//         taskModal.close();
-//     });
-//     return taskModal;
-// }
-
-// function editTaskForm(taskIndex, taskDesc) {
-//     const form = document.querySelector(".edit-task-form");
-//     const taskModal = document.querySelector(".edit-task-modal");
-//     form.addEventListener("submit", (e) => {
-//         e.preventDefault();
-//         const description = document.getElementById("edit-task-description");
-//         taskArr[taskIndex].description[taskDesc] = description.value;
-//         displayTask(taskArr[taskIndex].description);
-//         taskModal.close();
-//         form.reset();
-//         return taskArr;
-//     }, {once: true});
-// }
-
-// function deleteTask(taskIndex, descIndex) {
-//     return taskArr[taskIndex].description.splice(descIndex, 1);
-// }
-
-// function updateTask(index, newTitle) {
-//     taskArr[index].project = newTitle;
-//     displayTask(taskArr[index].description);
-//     console.log(taskArr[index]);
-//     return taskArr;
-// }
-
-// function checkTaskArray(title) {
-//     if(taskArr.length === 0) return false;
-//     for(let x = 0; x < taskArr.length; x++) {
-//         if(taskArr[x].project === title) return true;
-//     }
-// }
-
-// function strikeThrough(checkbox, el) {
-//     if(!checkbox.classList.contains("active")) {
-//         checkbox.style.backgroundColor = "#FFB647";
-//         el.style.textDecoration = "line-through";
-//         checkbox.classList.add("active");
-//     }else {
-//         checkbox.style.background = "none";
-//         el.style.textDecoration = "none";
-//         checkbox.classList.remove("active");
-//     }
-// }
-
-// const userTask = (project, description) => {
-//     return {project, description};
-// } 
-
-// export {taskModal, taskForm, taskArr, updateTask, editTaskModal, editTaskForm, deleteTask, strikeThrough};
-
 import { displayTask } from "./UI";
+import { oldProjectValue } from "./project";
 
 let taskArr = [];
 
@@ -144,6 +45,11 @@ function editTaskForm(projectTitle, taskTitle) {
     }, {once: true});
 }
 
+function updateTask(index, newTitle) {
+    taskArr[index].title = newTitle;
+    displayTask(taskArr[index].desc);
+}
+
 function deleteTask(projectTitle, taskTitle) {
     const index = findIndex(taskArr, projectTitle);
     const taskIndex = findTaskIndex(taskArr[index].desc, taskTitle);
@@ -182,4 +88,4 @@ function taskTitleMatch(title) {
     return false;
 }
 
-export {taskForm, selectedTask, deleteEntireTask, editTaskForm, deleteTask};
+export {taskForm, selectedTask, deleteEntireTask, editTaskForm, deleteTask, updateTask};
